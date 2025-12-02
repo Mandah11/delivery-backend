997d73835955c5a2eacb6e71e6c1ce78a761e66f
25LP8334 25LP8334 <25LP8334@G301-CC0196.local>
2025-12-02 16:08:50 +0800

    ll

--- src/routes/foodsOrder.js
+++ src/routes/foodsOrder.js
@@ -3,7 +3,6 @@ import { getFoodserverorder } from "../resolvers/foodorder/get-foodOrd.js";
 import { CreateFoodserverorder } from "../resolvers/foodorder/create-foodOrd.js";
 import { UpdateFoodserverorder } from "../resolvers/foodorder/update-foodOrd.js";
 import { DeleteFoodserverorder } from "../resolvers/foodorder/delete-foodOrd.js";
-import { getOrderByUserId } from "../resolvers/foodorder/get-userbyId.js";
 
 export const ordered = express.Router();
 
